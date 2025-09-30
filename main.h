ba00f5116c1c9a0708f7d5126229635bc8a46ee8
Reemabdu213 <11972@holbertonstudents.com>
2025-09-30 21:05:53 +0000

    Update main.h file

--- main.h
+++ main.h
@@ -6,5 +6,8 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
 
-#endif
+#endif /* MAIN_H */
