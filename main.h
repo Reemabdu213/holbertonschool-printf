73be1dde692554832188247d2afb1e61b810d6ea
Reemabdu213 <11972@holbertonstudents.com>
2025-09-30 23:24:03 +0300

    Update main.h
    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
-int _printf(const char *format,...);
+
+int _printf(const char *format, ...);
 int _putchar(char c);
-int specifiers_handler(char specifier,va_list args);
+
 #endif
