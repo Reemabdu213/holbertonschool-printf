bd59c5b289f8e70a99dac6e17afa9825f4ad8b88
bynoura <nr.abdullah@gmail.com>
2025-10-01 16:34:40 +0000

    22

--- functions.c
+++ functions.c
@@ -1,49 +1,56 @@
 #include "main.h"
+
 /**
-*_putchar - print a character
-*@c: character to print.
-*
-*Return: 1 for success
-*/
+ * _putchar - Prints a character to stdout.
+ * @c: The character to print.
+ *
+ * Return: 1 on success.
+ */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 /**
-*print_string - print a string
-*@str: string to print.
-*
-*Return: number of characters printed
-*/
+ * print_string - Prints a string to stdout.
+ * @str: The string to print.
+ *
+ * Return: Number of characters printed.
+ */
 int print_string(char *str)
 {
-int i = 0;
-if (!str)
-str = "(null)";
-while (*str)
-i += _putchar(*str++);
-return (i);
+	int i = 0;
+
+	if (!str)
+		str = "(null)";
+
+	while (*str)
+		i += _putchar(*str++);
+
+	return (i);
 }
 
 /**
-* print_unsigned - Print an unsigned int in base 10.
-* @n: The number to print.
-*
-* Return: Number of characters printed.
-*/
+ * print_unsigned - Prints an unsigned integer in base 10.
+ * @n: The number to print.
+ *
+ * Return: Number of characters printed.
+ */
 int print_unsigned(unsigned int n)
 {
-int count = 0;
-if (n / 10)
-count += print_unsigned(n / 10);
-count += _putchar((n % 10) + '0');
-return (count);
+	int count = 0;
+
+	if (n / 10)
+		count += print_unsigned(n / 10);
+
+	count += _putchar((n % 10) + '0');
+
+	return (count);
 }
 
 /**
- * print_number - Print a signed integer in base 10.
- * @n: The number to print (may be negative).
+ * print_number - Prints a signed integer in base 10.
+ * @n: The number to print (can be negative).
  *
  * Description: Handles INT_MIN safely by promoting to long before
  * converting to unsigned.
@@ -52,17 +59,20 @@ return (count);
  */
 int print_number(int n)
 {
-int count = 0;
-unsigned int u;
-if (n < 0)
-{
-count += _putchar('-');
-u = (unsigned int)(-(long)n);
-}
-else
-{
-u = (unsigned int)n;
-}
-count += print_unsigned(u);
-return (count);
+	int count = 0;
+	unsigned int u;
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		u = (unsigned int)(-(long)n);
+	}
+	else
+	{
+		u = (unsigned int)n;
+	}
+
+	count += print_unsigned(u);
+
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,10 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
+
+/* core */
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_string(char *str);
-int print_number(int n);
+
+/* specifier handlers */
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_percent(va_list ap);
 int print_unsigned(unsigned int n);
+
+/* helpers */
+int print_normal_char(char c);
+int handle_specifier(char c, va_list ap);
+
 #endif
