2b38df0a39cee927b49c49f9a73030beef4bdbcd
Reemabdu213 <11972@holbertonstudents.com>
2025-10-02 17:08:48 +0000

    Fix Betty style: replace leading spaces with tabs in handle_format

--- _printf.c
+++ _printf.c
@@ -67,24 +67,24 @@ static int print_int(int n)
  */
 static int handle_format(char c, va_list ap)
 {
-	if (c == 'c')
-		return (_putchar((char)va_arg(ap, int)));
-	if (c == 's')
-		return (print_text(va_arg(ap, char *)));
-	if (c == '%')
-		return (_putchar('%'));
-	if (c == 'd' || c == 'i')
-		return (print_int(va_arg(ap, int)));
-	if (c == 'b')
-	return (print_binary(va_arg(ap, unsigned int)));
-	if (c == 'u')
-    	return (print_uint(va_arg(ap, unsigned int)));
-	if (c == 'o')
-    	return (print_octal(va_arg(ap, unsigned int)));
-	if (c == 'x')
-    	return (print_hex_low(va_arg(ap, unsigned int)));
-	if (c == 'X')
-    	return (print_hex_up(va_arg(ap, unsigned int)));
+if (c == 'c')
+return (_putchar((char)va_arg(ap, int)));
+if (c == 's')
+return (print_text(va_arg(ap, char *)));
+if (c == '%')
+return (_putchar('%'));
+if (c == 'd' || c == 'i')
+return (print_int(va_arg(ap, int)));
+if (c == 'b')
+return (print_binary(va_arg(ap, unsigned int)));
+if (c == 'u')
+return (print_uint(va_arg(ap, unsigned int)));
+if (c == 'o')
+return (print_octal(va_arg(ap, unsigned int)));
+if (c == 'x')
+return (print_hex_low(va_arg(ap, unsigned int)));
+if (c == 'X')
+return (print_hex_up(va_arg(ap, unsigned int)));
 
 	/* Unknown specifier: print it raw */
 	_putchar('%');
