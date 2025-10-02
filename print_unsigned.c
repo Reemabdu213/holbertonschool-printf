86386d1abe6d51f5c19374c2fa752d3367dba325
Reemabdu213 <11972@holbertonstudents.com>
2025-10-02 17:15:36 +0000

    Docs: add Betty headers for print_uint, print_octal, print_hex_low, print_hex_up

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_unsigned_base - Print unsigned int in any base
- * @n: number (unsigned)
- * @base: radix (8, 10, 16)
- * @uppercase: non-zero -> A..F, zero -> a..f
+ * print_unsigned_base - Print an unsigned integer in a given base
+ * @n: number to print
+ * @base: radix to use (e.g. 8, 10, 16)
+ * @uppercase: if non-zero, use 'A'..'F'; otherwise 'a'..'f'
  *
  * Return: number of characters printed
  */
@@ -12,9 +12,6 @@ int print_unsigned_base(unsigned int n, unsigned int base, int uppercase)
 {
 	int total = 0;
 	unsigned int digit;
-
-	if (base < 2)
-		return (0);
 
 	if (n / base)
 		total += print_unsigned_base(n / base, base, uppercase);
@@ -28,7 +25,46 @@ int print_unsigned_base(unsigned int n, unsigned int base, int uppercase)
 	return (total);
 }
 
-int print_uint(unsigned int n)      { return (print_unsigned_base(n, 10, 0)); }
-int print_octal(unsigned int n)     { return (print_unsigned_base(n, 8, 0)); }
-int print_hex_low(unsigned int n)   { return (print_unsigned_base(n, 16, 0)); }
-int print_hex_up(unsigned int n)    { return (print_unsigned_base(n, 16, 1)); }
+/**
+ * print_uint - Print an unsigned integer in decimal (%u)
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_uint(unsigned int n)
+{
+	return (print_unsigned_base(n, 10, 0));
+}
+
+/**
+ * print_octal - Print an unsigned integer in octal (%o)
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_octal(unsigned int n)
+{
+	return (print_unsigned_base(n, 8, 0));
+}
+
+/**
+ * print_hex_low - Print an unsigned integer in lowercase hex (%x)
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_hex_low(unsigned int n)
+{
+	return (print_unsigned_base(n, 16, 0));
+}
+
+/**
+ * print_hex_up - Print an unsigned integer in uppercase hex (%X)
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_hex_up(unsigned int n)
+{
+	return (print_unsigned_base(n, 16, 1));
+}
